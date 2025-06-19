f0f0f972ef828d6950f4c66bb15d05e17ff64194
CodeRAFCE <seshan.io@prismscale.com>
2025-06-19 18:13:03 +0530

    Add project files for HelloWorldOldSyntax including .csproj and Program.cs

---
+++ HelloWorldOldSyntax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldOldSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! (Old Syntax)");
+        }
+    }
+}
