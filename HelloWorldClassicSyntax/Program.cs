8c8e4d7c375a892d160336fbdb8fcf619f44244c
CodeRAFCE <seshan.io@prismscale.com>
2025-07-01 09:44:08 +0530

    Add password generation functionality to HelloWorldClassicSyntax installed a NuGet Package into the project folder

--- HelloWorldClassicSyntax/Program.cs
+++ HelloWorldClassicSyntax/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PasswordGenerator;
 
 
 namespace HelloWorldClassicSyntax
@@ -8,11 +9,21 @@ namespace HelloWorldClassicSyntax
         static void Main(string[] args)
         {
             PrintHelloWorld();
+            GenerateAndPrintPassword();
         }
 
         static void PrintHelloWorld()
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        static void GenerateAndPrintPassword()
+        {
+            // Create a password generator instance
+            var pwd = new Password();
+            // Generate a random password (default length is 8)
+            string password = pwd.Next();
+            Console.WriteLine($"Generated Password: {password}");
         }
     }
 }
